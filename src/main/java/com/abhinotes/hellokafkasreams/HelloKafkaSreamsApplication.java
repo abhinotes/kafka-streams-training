b09c0da96a2e897ad51a815f72ae2c39a73799f7
Abhishek Kumar <gabhishekkumar@outlook.com>
2021-02-21 20:13:45 +0530

    Initial Spring boot project skeleton with dependencies in pom

---
+++ src/main/java/com/abhinotes/hellokafkasreams/HelloKafkaSreamsApplication.java
@@ -0,0 +1,13 @@
+package com.abhinotes.hellokafkasreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloKafkaSreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloKafkaSreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhinotes/hellokafkasreams/HelloKafkaSreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinotes.hellokafkasreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloKafkaSreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
